0a6b940974fda3b500d767de0e2505f82b99c8af
6704062610291 <s6704062610291@email.kmutnb.ac.th>
2024-12-03 15:14:19 +0700

    Add files via upload

---
+++ printex.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	double commission = 5000.00;
+	double percent = 0.05;
+	int no_of_day = 7;
+	
+	printf("1\t2\t3\t4\t5\t6\t7\t8\t9\t10\t11\t12\t13\t14\t15");
+	printf("\tr\ta\tt\te\t:\t\t\t\t0\t.\t0\t5\t%%\n");
+	printf("c\to\tm\tm\t\t\t:\t5\t0\t0\t0\t.\t0\t0\n");
+	printf("\n");
+	printf("n\to\t.\to\tf\t\td\ta\ty\t:\t%d\td\ta\ty\n", no_of_day);
+}
